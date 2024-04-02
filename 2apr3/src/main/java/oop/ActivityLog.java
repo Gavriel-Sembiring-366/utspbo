6d12f37c992ac651c3452b0228c6e24363500873
Healm <116085297+Agent-Healm@users.noreply.github.com>
2024-04-02 19:32:43 +0700

    Add files via upload

---
+++ 2apr3/src/main/java/oop/ActivityLog.java
@@ -0,0 +1,47 @@
+package oop;
+
+public class ActivityLog {
+    public int idAct;
+    public int idUser;
+    public String date;
+    public String time;
+    public String username;
+    public String typeAct;
+    public String desc;
+
+    // constructor
+    public ActivityLog(int idAct, int idUser, String date, String time, String username, String typeAct, String desc){
+        this.idAct = idAct;
+        this.idUser = idUser;  
+        this.date = date;
+        this.time = time;
+        this.username = username;
+        this.typeAct = typeAct;
+        this.desc = desc;
+    }
+    public ActivityLog(int idUser, String date, String username, String typeAct){
+        this.idUser = idUser;  
+        this.date = date;
+        this.username = username;
+        this.typeAct = typeAct;
+    }
+    public ActivityLog(){}
+    
+    // Getters
+    public int getIdAct() {return idAct;}
+    public String getDate() {return date;}
+    public String getUsername() {return username;}
+    public String getTypeAct() {return typeAct;}
+    public String getTime() {return time;}
+    public int getIdUser() {return idUser;}
+    public String getDesc() { return desc;}
+
+    // Setters
+    public void setIdAct(int idAct) {this.idAct = idAct;}
+    public void setIdUser(int idUser) {this.idUser = idUser;}
+    public void setDate(String date) {this.date = date;}
+    public void setTime(String time) {this.time = time;}
+    public void setUsername(String username) {this.username = username;}
+    public void setTypeAct(String typeAct) {this.typeAct = typeAct;}
+    public void setDesc(String desc) {this.desc = desc;}
+}
---
+++ 2apr3/src/main/java/oop/AdminTransLogController.java
@@ -0,0 +1,139 @@
+package oop;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+
+public class AdminTransLogController {
+    
+    @FXML public TableView<TransactionLog> tableViewItem;
+    @FXML private TextField textFieldTransLogId;
+    @FXML private TextField textFieldUserId;
+    @FXML private TextField textFieldDatetime;
+    @FXML private TextField textFieldUsername;
+    @FXML private TextField textFieldItem;
+    @FXML private TextField textFieldTotal;
+    @FXML private TextField textFieldSearchItem;
+    @FXML private Label labelTitle;
+    @FXML private Label labelDate;
+    @FXML private Label labelView;
+    @FXML private Label labelTransLogId;
+    @FXML private Label labelUserId;
+    @FXML private Label labelDatetime;
+    @FXML private Label labelUsername;
+    @FXML private Label labelItem;
+    @FXML private Label labelTotal;
+    @FXML private Label labelSelectItem;
+    @FXML private Label labelDescription;
+    @FXML private Button buttonBatal;
+    @FXML private Button buttonSwitch;
+
+    public void initialize(){
+        
+        ObservableList<Label> labels = FXCollections.observableArrayList(
+            labelView,
+            labelDescription,
+            labelTransLogId,
+            labelUserId,
+            labelDatetime,
+            labelUsername,
+            labelItem,
+            labelTotal,
+            labelSelectItem
+        );
+
+        ObservableList<TextField> textFields = FXCollections.observableArrayList(
+            textFieldTransLogId,
+            textFieldUserId,
+            textFieldDatetime,
+            textFieldUsername,
+            textFieldItem,
+            textFieldTotal,
+            textFieldSearchItem
+        );
+
+        ObservableList<Button> buttons = FXCollections.observableArrayList(
+            buttonBatal,
+            buttonSwitch
+        );
+
+        labels.forEach(label -> {label.fontProperty().set(App.Montserrat(14.0));
+            
+        });
+        buttons.forEach(button->{button.fontProperty().set(App.Montserrat(18.0));});
+        textFields.forEach(textField ->{textField.fontProperty().set(App.Montserrat(12.0));});
+        labelTitle.fontProperty().set(App.Montserrat(56.0));
+
+        tableViewItem.getColumns().addAll(Item.getTransLogColumns("admin_trans_log"));
+        DBConnector.initDBConnection();
+        loadTransLogTable();
+    }
+
+    private void loadTableViewListener(){
+        tableViewItem.setOnMouseClicked((MouseEvent event)->{
+            int index = tableViewItem.getSelectionModel().getSelectedIndex();
+            TransactionLog row = tableViewItem.getItems().get(index);
+
+            String currentTime = (row.date).split(" ")[1];
+            textFieldTransLogId.setText(Integer.toString(row.transactionID));
+            textFieldUserId.setText(Integer.toString(row.userID));
+            textFieldDatetime.setText(currentTime);
+            textFieldUsername.setText(row.username);
+            textFieldItem.setText(row.item);
+            textFieldTotal.setText(Float.toString(row.total));
+        });
+    }
+
+    private void loadTransLogTable(){
+        try {
+            TransactionLogDB.fetchAllTransactionLog().forEach(log->{
+                tableViewItem.getItems().add(log);
+            });
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error :" + e);
+        }
+        loadTableViewListener();
+    }
+
+    private void reloadTable(String selectedItem){
+        List<TransactionLog> transLogs = TransactionLogDB.getFilteredTransactionLogs(selectedItem);
+        if (transLogs.isEmpty()){
+            loadTransLogTable();
+        }
+        else{
+            transLogs.forEach(log->{
+                tableViewItem.getItems().add(log);
+            });
+            loadTableViewListener();
+        }
+    }
+
+    @FXML
+    private void actionExit() throws IOException{
+        App.setRoot("login");
+    }
+
+    @FXML 
+    private void actionFindItem() throws IOException, SQLException{
+        tableViewItem.getItems().clear();
+        String selectedItem = textFieldSearchItem.getText();
+        reloadTable(selectedItem);
+    }
+
+    @FXML
+    private void actionSwitch() throws IOException {
+        App.setRoot("adminItem");
+    }
+}
